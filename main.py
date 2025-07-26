9d737806509c4123e709f422836356a9679df990
Geerthanah Mathivathanan <138343450+geerthanah@users.noreply.github.com>
2025-07-26 18:15:41 +0530

    update main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from core.engine import AutoCompleteEngine
+
+if __name__ == "__main__":
+    engine = AutoCompleteEngine()
+    engine.load_words("data/words.txt")
+
+    while True:
+        prefix = input("\n🔍 Enter search prefix (or 'exit'): ").strip().lower()
+        if prefix == 'exit':
+            break
+        suggestions = engine.get_suggestions(prefix)
+        print(f"Suggestions: {', '.join(suggestions) if suggestions else 'No matches found'}")
